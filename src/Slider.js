f22d8ea0bcfd8de008dfc82b2e7cf4209df5c5a3
jolstorm <gaurav.virmani@sparkeighteen.com>
2022-07-18 05:40:43 +0530

    Fix:netlify error

--- src/Slider.js
+++ src/Slider.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import Card from "./Card";
 import Right from "./images/Right-arrow.svg";
 
