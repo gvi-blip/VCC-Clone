9bce1d49249b52f97129fe4b6f4eeb492ca36358
jolstorm <gaurav.virmani@sparkeighteen.com>
2022-10-02 19:47:06 +0530

    Fixed h tag cannot be empty issue

--- src/components/loadingSkeleton/loadingSkeleton.jsx
+++ src/components/loadingSkeleton/loadingSkeleton.jsx
@@ -9,8 +9,8 @@ function LoadingSkeleton() {
     <div className="container">
         <div className="text-content">
             <p className="skeleton-box" style={{width:" 50%", height:22}}></p>
-            <h1 className="skeleton-box" style={{width:"100%", height:35}}></h1>
-            <h1 className="skeleton-box" style={{width:"70%", height:35, marginTop: 0}}></h1>
+            <h1 className="skeleton-box" style={{width:"100%", height:35}}>{" "}</h1>
+            <h1 className="skeleton-box" style={{width:"70%", height:35, marginTop: 0}}>{" "}</h1>
             <p className="skeleton-box" style={{width:"100%", height:28, marginTop:0}}></p>
             <p className="skeleton-box" style={{width:"70%", height:28}}></p>
             <p style={{width:"60%", height:15, display:"flex", alignItems: "center"}}><span className="skeleton-box" style={{width:"30%",height:"100%"}}></span><span className="skeleton-box" style={{width:10, height:10, borderRadius:"50%"}}></span><span className="skeleton-box"style={{width:"45%",height:'100%'}}></span></p>
